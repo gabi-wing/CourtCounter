2bb7bb6da6f0f4c991a47f04f1d4119c43e2ed57
Gabi Wing <gwing2004@gmail.com>
2022-09-13 12:57:26 -0500

    added java code

--- app/src/main/java/com/example/pracriceset/MainActivity.java
+++ app/src/main/java/com/example/pracriceset/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     int teamAScore = 0;
 
     public void addOneForTeamA(View v) {
@@ -23,17 +24,46 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void addTwoForTeamA(View v) {
-        teamAScore+=2;
+        teamAScore += 2;
         displayForTeamA(teamAScore);
     }
 
     public void addThreeForTeamA(View v) {
-        teamAScore+=3;
+        teamAScore += 3;
         displayForTeamA(teamAScore);
     }
 
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
+    }
+
+    int teamBScore = 0;
+
+    public void addOneForTeamB(View v) {
+        teamBScore++;
+        displayForTeamB(teamBScore);
+    }
+
+    public void addTwoForTeamB(View v) {
+        teamBScore+=2;
+        displayForTeamB(teamBScore);
+    }
+
+    public void addThreeForTeamB(View v) {
+        teamBScore+=3;
+        displayForTeamB(teamBScore);
+    }
+
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void reset(View v){
+        teamAScore = 0;
+        teamBScore = 0;
+        displayForTeamA(teamAScore);
+        displayForTeamB(teamBScore);
     }
 }
